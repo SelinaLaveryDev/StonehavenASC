bb52f2e3d6c87386f67fe8cfe1f21011137ebe2d
Selina Lavery <selina118@hotmail.com>
2023-11-01 07:47:54 +0000

    material ui and sidebar data component

---
+++ src/Components/SidebarData.js
@@ -0,0 +1,26 @@
+export const SidebarData = [
+	{
+		title: "Home",
+		path: "/",
+		icon: "fas fa-home",
+		cName: "nav-text",
+	},
+	{
+		title: "About",
+		path: "/about",
+		icon: "fas fa-address-card",
+		cName: "nav-text",
+	},
+	{
+		title: "Projects",
+		path: "/projects",
+		icon: "fas fa-project-diagram",
+		cName: "nav-text",
+	},
+	{
+		title: "Contact",
+		path: "/contact",
+		icon: "fas fa-envelope",
+		cName: "nav-text",
+	},
+];
