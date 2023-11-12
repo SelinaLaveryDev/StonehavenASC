bd66eb49a4f14c0588587c2790db6eafaa502c07
Selina Lavery <selina@kionnali.com>
2023-11-12 23:27:01 +0000

    re-styled home page without carousel

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,34 +1,40 @@
+import RedBanner from "../Assets/RedBanner.png";
+import SASCfullTitle from "../Assets/SASCfullTitle.png";
+import FrontCrawl from "../Assets/Photos/website-photos/FrontCrawl.png";
+import WelcomeSASC from "../Assets/WelcomeSASC.png";
 import "../Components/Home.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { photoData } from "../Components/PhotoData.js";
 
 export default function Home() {
-	const settings = {
-		dots: true,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-	};
 	return (
 		<div className="home-container">
-			<div className="photo-carousel"></div>
-			<Slider {...settings}>
-				{photoData.map((item) => {
-					return (
-						<div
-							key={item.id}
-							className="photo-container">
-							<img
-								src={item.img}
-								alt={item.title}
-							/>
-						</div>
-					);
-				})}
-			</Slider>
+			<img
+				className="welcome-banner"
+				src={RedBanner}
+				alt="welcome banner"
+			/>
+			<img
+				className="sasc-title"
+				src={SASCfullTitle}
+				alt="Stonehaven Amateur Swimming Club"
+			/>
+			<img
+				className="front-crawl"
+				src={FrontCrawl}
+				alt="Swimmer doing front crawl"
+			/>
+			<div className="welcome-section">
+				<img
+					src={WelcomeSASC}
+					alt="Welcome to Stonehaven Amateur Swimming Club"
+				/>
+				<p>
+					Stonehaven Amateur Swimming Club (SASC) is a competitive swimming club
+					based in Stonehaven, Aberdeenshire. We are a small, friendly club with
+					a focus on developing swimmers to their full potential. We welcome
+					swimmers of all ages and abilities, from those who have just learned
+					to swim to those who compete at national level.
+				</p>
+			</div>
 		</div>
 	);
 }
