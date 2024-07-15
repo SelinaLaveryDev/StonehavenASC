205eefbf1ffda02bc63c2db3882f1d613f930a98
Selina <selina@kionnali.com>
2024-07-15 22:44:48 +0100

    top nav fix

--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -3,7 +3,6 @@ import './TopNav.css'; // Make sure to create TopNav.css for styling
 import SASC_logo1 from '../Assets/SASC_logo1 - Edited 1.png';
 import { Link } from 'react-router-dom';
 
-
 const TopNav = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -11,24 +10,26 @@ const TopNav = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="top-nav">
       <div className='menu-row'>
-      <div className="menu-icon" onClick={toggleMenu}>
-      {isOpen ? '\u2716' : '\u2630'} {/* Unicode character for hamburger icon */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          {isOpen ? '\u2716' : '\u2630'} {/* Unicode character for hamburger icon */}
         </div>
-        <Link to="/"><img src={SASC_logo1} alt="Welcome to Stonehaven Amateur Swimming Club" width={100}/></Link>
+        <Link to="/"><img src={SASC_logo1} alt="Welcome to Stonehaven Amateur Swimming Club" width={100} /></Link>
       </div>
 
-     
       <ul className={`menu ${isOpen ? "show" : ""}`}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/events">Events / News</Link></li>
-        <li><Link to="/people">People</Link></li>
-        <li><Link to="/links">Links</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-
+        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+        <li><Link to="/about" onClick={closeMenu}>About</Link></li>
+        <li><Link to="/events" onClick={closeMenu}>Events / News</Link></li>
+        <li><Link to="/people" onClick={closeMenu}>People</Link></li>
+        <li><Link to="/links" onClick={closeMenu}>Links</Link></li>
+        <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
       </ul>
     </nav>
   );
